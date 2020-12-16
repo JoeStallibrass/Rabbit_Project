64f1884437cc2b2a54da94822e3c242c66df9213
hbouteba <hbouteba@spartaglobal.com>
2020-12-16 11:30:44 +0000

    group 1 rabbit classes and tests

--- rabbit_project/Male_Rabbit.py
+++ rabbit_project/Male_Rabbit.py
@@ -1,1 +1,13 @@
 # very similar to base rabbit class but has an assigned sex as male
+from rabbit_project.Rabbit import Rabbit
+
+
+class MaleRabbit(Rabbit):
+
+    # inherits from rabbit class
+    def __init__(self):
+        super().__init__()
+        self.sex = "M"
+
+
+
